f0c55d3b8b71419ee3b9983daf0a9248d0c3050a
goksunberen <goksunberen@gmail.com>
2021-03-22 20:44:20 +0300

    Update time-conversion.py

--- time-conversion.py
+++ time-conversion.py
@@ -19,11 +19,7 @@ def timeConversion(s):
 
 if __name__ == '__main__':
     f = open(os.environ['OUTPUT_PATH'], 'w')
-
     s = input()
-
     result = timeConversion(s)
-
     f.write(result + '\n')
-
     f.close()
