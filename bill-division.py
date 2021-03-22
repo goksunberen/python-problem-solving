6a34280ef75d1d60b1fb5b33d19c857a333a64e7
goksunberen <goksunberen@gmail.com>
2021-03-22 20:42:47 +0300

    Update bill-division.py

--- bill-division.py
+++ bill-division.py
@@ -13,13 +13,9 @@ def bonAppetit(bill, k, b):
 
 if __name__ == '__main__':
     nk = input().rstrip().split()
-
     n = int(nk[0])
-
     k = int(nk[1])
-
     bill = list(map(int, input().rstrip().split()))
-
     b = int(input().strip())
 
     bonAppetit(bill, k, b)
