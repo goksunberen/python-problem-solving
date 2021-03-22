81dbe50363575ff41f4a9f9190f8aa82fd5a532d
goksunberen <goksunberen@gmail.com>
2021-03-22 20:43:50 +0300

    Update subarray-division.py

--- subarray-division.py
+++ subarray-division.py
@@ -14,19 +14,12 @@ def birthday(s, d, m):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     n = int(input().strip())
-
     s = list(map(int, input().rstrip().split()))
-
     dm = input().rstrip().split()
-
     d = int(dm[0])
-
     m = int(dm[1])
-
     result = birthday(s, d, m)
-
     fptr.write(str(result) + '\n')
 
     fptr.close()
